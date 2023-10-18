0bbd84cf100e9417fd877ba16b13387ab1af96d3
beckybarton <nickielou.barton@c-one.ph>
2023-10-18 19:54:37 +0800

    routes updated with auth

--- routes/web.php
+++ routes/web.php
@@ -19,36 +19,34 @@ use App\Http\Controllers\LoginController;
 |
 */
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard.index');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard.index')->middleware('auth');
 Route::get('/', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard.index');
 
 // ITEMS
-// Route::post('/items/store', [ItemController::class, 'store'])->name('item.store')->middleware('auth');
-Route::post('/items/store', [ItemController::class, 'store'])->name('item.store');
-Route::get('/items', [ItemController::class, 'index'])->name('item.index');
-// Route::put('/vehicle-types/{id}', [VehicleTypeController::class, 'update'])->name('vehicle-types.update')->middleware('auth');
-Route::put('/item/{id}', [ItemController::class, 'update'])->name('item.update');
-Route::post('/categories/store', [ItemController::class, 'storecategory'])->name('item.storecategory');
+Route::post('/items/store', [ItemController::class, 'store'])->name('item.store')->middleware('auth');
+Route::get('/items', [ItemController::class, 'index'])->name('item.index')->middleware('auth');
+Route::put('/item/{id}', [ItemController::class, 'update'])->name('item.update')->middleware('auth');
+Route::post('/categories/store', [ItemController::class, 'storecategory'])->name('item.storecategory')->middleware('auth');
 
 // LOCATIONS
-Route::post('/locations/store', [LocationController::class, 'store'])->name('location.store');
-Route::get('/locations', [LocationController::class, 'index'])->name('location.index');
+Route::post('/locations/store', [LocationController::class, 'store'])->name('location.store')->middleware('auth');
+Route::get('/locations', [LocationController::class, 'index'])->name('location.index')->middleware('auth');
 
 // CUSTOMERS
-Route::post('/customers/store', [CustomerController::class, 'store'])->name('customer.store');
+Route::post('/customers/store', [CustomerController::class, 'store'])->name('customer.store')->middleware('auth');
 
 // USERS
-Route::post('/users/store', [UserController::class, 'store'])->name('user.store');
-Route::get('/users', [UserController::class, 'index'])->name('user.index');
+Route::post('/users/store', [UserController::class, 'store'])->name('user.store')->middleware('auth');
+Route::get('/users', [UserController::class, 'index'])->name('user.index')->middleware('auth');
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
 // POS
-Route::get('/pos', [PosController::class, 'index'])->name('pos.index');
-Route::get('/search-items', [PosController::class, 'searchItems'])->name('pos.searchItems');
-Route::get('/search-customers', [PosController::class, 'searchCustomers'])->name('pos.searchCustomers');
-Route::get('/get-customer-credit', [PosController::class, 'getCustomerCredit'])->name('pos.getCustomerCredit');
-Route::post('/save-order', [PosController::class, 'saveOrder'])->name('pos.saveOrder');
-Route::get('/all-receivables', [PosController::class, 'allReceivables'])->name('pos.allReceivables');
-Route::get('/customer-receivables', [PosController::class, 'customerReceivables'])->name('pos.customerReceivables');
-Route::post('/receive-payment', [PosController::class, 'receivepayment'])->name('pos.receivepayment');
+Route::get('/pos', [PosController::class, 'index'])->name('pos.index')->middleware('auth');
+Route::get('/search-items', [PosController::class, 'searchItems'])->name('pos.searchItems')->middleware('auth');
+Route::get('/search-customers', [PosController::class, 'searchCustomers'])->name('pos.searchCustomers')->middleware('auth');
+Route::get('/get-customer-credit', [PosController::class, 'getCustomerCredit'])->name('pos.getCustomerCredit')->middleware('auth');
+Route::post('/save-order', [PosController::class, 'saveOrder'])->name('pos.saveOrder')->middleware('auth');
+Route::get('/all-receivables', [PosController::class, 'allReceivables'])->name('pos.allReceivables')->middleware('auth');
+Route::get('/customer-receivables', [PosController::class, 'customerReceivables'])->name('pos.customerReceivables')->middleware('auth');
+Route::post('/receive-payment', [PosController::class, 'receivepayment'])->name('pos.receivepayment')->middleware('auth');
